ecc402a66edeae00aaada1ccf28d51aeb77e51e5
biel-dev <biel.castro.dev>
2022-03-12 11:39:46 -0300

    first commit

---
+++ calculadora/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Calculator from './main/Calculator.jsx'
+
+ReactDOM.render(
+  <>
+    <h1>CALCULADORA</h1>
+    <Calculator/>
+  </>
+, document.getElementById('root'))
